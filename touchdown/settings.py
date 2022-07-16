88ac0c9595766b45f0edfda769e8e354a0970a6d
Deji <wrecodde@gmail.com>
2022-07-17 00:53:24 +0100

    changes to user accounts model
    big change, under the hood.
    the touchdown project was deleted and recreated, to clear out existing
    migrations and possible relationships.
    not tracked here, but the db was recreated too
    nb: https://simpleisbetterthancomplex.com/article/2021/07/08/what-you-should-know-about-the-django-user-model.html

--- touchdown/settings.py
+++ touchdown/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "accounts"
 ]
 
 MIDDLEWARE = [
@@ -107,6 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = "accounts.CustomUser"
 
 
 # Internationalization
