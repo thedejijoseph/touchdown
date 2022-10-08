66a05a6fc4b58c2382edb88661db4e4e90a7dba3
Deji <thedejijoseph@gmail.com>
2022-10-08 19:53:52 +0100

    add: setup for account authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,7 @@ from accounts import views
 urlpatterns = [
     path('', views.AccountsList.as_view()),
     path('verify', views.VerifyView.as_view()),
+    path('authenticate', views.AuthenticateAccountView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
